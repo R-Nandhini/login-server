b6f7260d497f75916a4a354c4e7a5607e56de3c1
R-Nandhini <32263315+R-Nandhini@users.noreply.github.com>
2023-11-12 13:26:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api",router);
 // public
 // get user
 app.get('/', (req, res) => {
-    res.send('Hello World, Welcome to Nandys World ');
+    res.send('Hello World, Nandys World ');
   });
 
 app.post("/createtable", async (req, res) => {
